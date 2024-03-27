6cecbcfae37b8402fc179f69e983d1f2a0042c37
kabust <tulyulyuk91@gmail.com>
2024-03-27 10:36:33 +0100

    feat: added default permissions settings

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,9 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "theatre.permissions.IsAdminOrIfAuthenticatedReadOnly",
     )
 }
 
