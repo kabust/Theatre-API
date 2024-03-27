278631137d401319d9873c1d6bbb6c73b00a4048
kabust <tulyulyuk91@gmail.com>
2024-03-27 10:11:27 +0100

    feat: added custom user model with email login

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,13 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+AUTH_USER_MODEL = "user.User"
 
 # Application definition
 
