bd2b81527860e2cf098458c916da66f5c8b915df
kabust <tulyulyuk91@gmail.com>
2024-03-28 11:51:08 +0100

    refactor: changed acces token lifetime to 10 minutes

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
